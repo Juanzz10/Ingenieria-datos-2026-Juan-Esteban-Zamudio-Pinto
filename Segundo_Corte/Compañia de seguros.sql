227dbe6dc9fc4198efa4faaaab863a2ff4d2f4a7
Juanzz10 <168881801+Juanzz10@users.noreply.github.com>
2026-03-15 21:13:59 -0500

    se sube script de sql compañia seguros

---
+++ Segundo_Corte/Compañia de seguros.sql
@@ -0,0 +1,48 @@
+create database compañiaseguros;
+use compañiaseguros;
+
+create table compañia(
+idCompañia varchar (50) primary key,
+nit varchar (20) unique not null,
+nombre varchar (50) not null,
+fechaFundacion date null,
+representanteLegal varchar (50) not null
+);
+
+create table seguros(
+idSeguro varchar (50) primary key,
+fechaInicio date null,
+fechaExperiacion date not null,
+estado varchar (20) not null,
+costo double not null,
+valorAsegurado double not null,
+idCompañiaFK varchar (50) not null,
+idAutomovilFK varchar (50) not null
+);
+
+create table automovil(
+idAutomovil varchar (50),
+marca varchar (20) not null,
+modelo varchar (20) not null,
+placa varchar (20) unique not null,
+tipos varchar (50) not null,
+añoFabricacion int null,
+serialChasis varchar (50) unique not null,
+pasajeros int not null,
+cilindraje int not null
+);
+
+create table involucrados(
+idAutomovilFK varchar (50) not null,
+idAccidenteFK varchar (50) not null
+);
+
+create table accidente(
+idAccidente varchar (50) primary key,
+automotores varchar (20) not null,
+fatalidades int not null,
+heridos int not null,
+lugar varchar (50) not null,
+fechaAccidente date not null
+);
+
